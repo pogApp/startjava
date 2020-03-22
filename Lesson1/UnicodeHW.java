4595ed012d860957c7aa96a23e540d50be967d50
pugApp <arbitmon@gmail.com>
2020-03-22 19:51:40 +0700

    Homework Unicode

---
+++ Lesson1/UnicodeHW.java
@@ -0,0 +1,10 @@
+public class UnicodeHW{
+    public static void main(String[] args) {
+      int i = 33;
+      while (i < 126){
+      System.out.print ((char)i + ", ");
+      i++;
+      }
+      System.out.print ((char)126);
+    }
+} 
