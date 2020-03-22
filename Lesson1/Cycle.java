35250c4b2dbd12fc128f7d214118ca7fa748798a
pugApp <arbitmon@gmail.com>
2020-03-22 19:35:25 +0700

    HW3

---
+++ Lesson1/Cycle.java
@@ -0,0 +1,28 @@
+public class Cycle{
+    public static void main(String[] args) {
+    	System.out.println("For");
+	for (int i=0;i<21;i++){
+		System.out.print(i+", ");
+	}  
+	System.out.println();  
+   	System.out.println("While");
+    int i = 6;
+    while (i>=-6) {
+    	System.out.print(i+", ");
+    	i--;
+    }
+    
+    System.out.println();  
+   	System.out.println("Do While");
+   	i = 10;
+    int sum = 0;
+   	do{
+   		if (i % 2 == 1){
+        sum += i;
+      }
+      i++;
+   	}
+    while (i<=20);
+  System.out.println("Sum = "+sum);
+    }
+} 
