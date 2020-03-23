4ba4d325459751524d18b65216ae0f9d8b51caa0
pugApp <arbitmon@gmail.com>
2020-03-23 20:56:17 +0700

    Variable upd 1 23.03

--- Lesson1/Variable.java
+++ Lesson1/Variable.java
@@ -1,4 +1,4 @@
-public class Variable{
+public class Variable {
     public static void main(String[] args) {
     	byte videoInput = 1;
     	short ram = 16;
@@ -6,8 +6,8 @@ public class Variable{
     	long videoFreq = 1500;
     	float procFreq = 2.8f;
     	double audioForm = 2.1d;
-    	char systemDisk = 'C' ;
+    	char systemDisk = 'C';
     	boolean isWindows = true;
-    	System.out.println (videoInput+", "+ram+", "+audioInput+", "+videoFreq+", "+procFreq+", "+audioForm+", "+systemDisk+", "+isWindows);
+    	System.out.println(videoInput + ", " + ram + ", " + audioInput + ", " + videoFreq + ", " + procFreq + ", " + audioForm + ", " + systemDisk + ", " + isWindows);
     }
 } 
