3632f89b5144b1d73e1d9c513f832227d2edc148
pugApp <arbitmon@gmail.com>
2020-03-24 23:36:39 +0700

    GuessNumber. Auto selector number

---
+++ Lesson1/GuessNumber2.java
@@ -0,0 +1,21 @@
+public class GuessNumber2 {
+    public static void main(String[] args) {
+        int randomNum = 72;
+        int playerNum = 50;
+        int leftBorder = 1;
+        int rightBorder = 100;
+
+        while (playerNum != randomNum) {
+            if (playerNum > randomNum) {
+                System.out.println("Your number " + playerNum + " is bigger that computer random number");
+                rightBorder = playerNum;
+                playerNum = (playerNum + leftBorder) / 2;
+            } else {
+                System.out.println("Your number " + playerNum + " is smaller that computer random number");
+                leftBorder = playerNum;
+                playerNum = (playerNum + rightBorder) / 2;
+            }
+        }
+        System.out.println("You are right! It is " + playerNum);
+    }
+}
