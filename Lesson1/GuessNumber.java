46e2ac5a98e6daae3bad6dd49cbeb9baba3f0a7d
pugApp <arbitmon@gmail.com>
2020-03-24 23:17:32 +0700

    GuessNumber upd2

--- Lesson1/GuessNumber.java
+++ Lesson1/GuessNumber.java
@@ -3,7 +3,7 @@ public class GuessNumber {
         int randomNum = 60;
         int playerNum = 60;
 
-        if (ourNum > randomNum) {
+        if (playerNum > randomNum) {
             System.out.println("Your number is bigger that computer random number");
         } else if (playerNum < randomNum) {
             System.out.println("Your number is smaller that computer random number");
