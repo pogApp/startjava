b8fcf4146af95b41b6a10f32dfeeeadd17ff2a27
pugApp <arbitmon@gmail.com>
2020-04-16 14:42:15 +0700

    Homework Jaeger v.1

---
+++ Lesson2/Jaeger.java
@@ -0,0 +1,72 @@
+public class Jaeger {
+
+	private String mark;
+	private double height;
+	private double weight;
+	private int speed;
+	private int strength;
+	private String weapon;
+
+	void setMark(String mark) {
+		this.mark = mark;
+	}
+
+	public String getMark() {
+		return mark;
+	}
+
+	void setHeight(double height) {
+		this.height = height;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	void setStrength(int strength) {
+		this.strength = strength;
+	}
+
+	public int getStrength() {
+		return strength;
+	}
+
+	void setWeapon(String weapon) {
+		this.weapon = weapon;
+	}
+
+	public void useWeapon() {
+		if (weapon != "null" && mark != "null") {
+			System.out.println(mark + " started using " + weapon);
+		} else {
+			System.out.println("Unknown mark of the robot or robot don't have any weapon");
+		}
+	}
+
+	public void move() {
+		if (speed > 0) {
+			System.out.println(mark + " started moving with speed " + speed);
+		} else if (speed < 0 ) {
+			System.out.println("This robot has wrong speed!");
+		} else {
+			System.out.println("This robot has speed 0. It can't move.");
+		}
+	}
+
+}
---
+++ Lesson2/JaegerTest.java
@@ -0,0 +1,41 @@
+public class JaegerTest {
+	
+	public static void main(String[] args) {
+
+		Jaeger robot1 = new Jaeger();
+		robot1.setMark("Mark6");
+		robot1.setHeight(81.77d);
+		robot1.setWeight(2004.0d);
+		robot1.setSpeed(10);
+		robot1.setStrength(9);
+		robot1.setWeapon("Elbow Rocket 2.0");
+
+		System.out.println("Robot 1");
+		System.out.println("Mark: " + robot1.getMark());
+		System.out.println("Height: " + robot1.getHeight() + "m");
+		System.out.println("Weight: " + robot1.getWeight() + "tons");
+		System.out.println("Speed: " + robot1.getSpeed());
+		System.out.println("Strength: " + robot1.getStrength());
+
+		robot1.move();
+		robot1.useWeapon();
+
+		Jaeger robot2 = new Jaeger();
+		robot2.setMark("Mark3");
+		robot2.setHeight(79.25d);
+		robot2.setWeight(1980.0d);
+		robot2.setSpeed(7);
+		robot2.setStrength(8);
+		robot2.setWeapon("Plasma Cannon");
+
+		System.out.println("Robot 2");
+		System.out.println("Mark: " + robot2.getMark());
+		System.out.println("Height: " + robot2.getHeight() + "m");
+		System.out.println("Weight: " + robot2.getWeight() + "tons");
+		System.out.println("Speed: " + robot2.getSpeed());
+		System.out.println("Strength: " + robot2.getStrength());
+
+		robot2.move();
+		robot2.useWeapon();
+	}
+}
