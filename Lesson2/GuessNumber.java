ab0ace934e384bcc33399b8b7270ffecf02a15b2
pugApp <arbitmon@gmail.com>
2020-04-17 17:27:19 +0700

    Game v1.1

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -1,41 +1,52 @@
 import java.util.Scanner;
+
 public class GuessNumber {
-	private int randomNumber;
-	public GuessNumber(int y) {
-		randomNumber = y;
+	private int randomNumber = (int) (Math.random() * 101);
+	String p1Name;
+	String p2Name;
+
+	public GuessNumber(String p1, String p2) {
+		p1Name = p1;
+		p2Name = p2;
 	}
+
 	private String resume = "yes";
+	int p1Num;
+	int p2Num;
+	Scanner scan = new Scanner(System.in);
 
-	public void setResume(String resume) {
-		this.resume = resume;
-	}
-
-	public String getResume() {
-		return resume;
-	}
-
-	public void check(int p1Num, int p2Num, String p1Name, String p2Name) {
-		if(p1Num == randomNumber && p2Num == randomNumber){
-			System.out.println("Both of you are right! It is " + randomNumber + ". Congratulations!");
-			resume = "no";
-		} else if (p1Num == randomNumber) {
-			System.out.println(p1Name + ", you are right! It is " + randomNumber + ". Congratulations!");
-			resume = "no";
-		} else if (p2Num == randomNumber) {
-			System.out.println(p2Name + ", you are right! It is " + randomNumber + ". Congratulations!");
-			resume = "no";
-		} else {
-			if (p1Num > randomNumber) {
-				System.out.println(p1Name + ", your number is too big");
+	public void game() {
+		while ("yes".equals(resume)) {
+			System.out.println(p1Name + ", enter the number:");
+			p1Num = scan.nextInt();
+			System.out.println(p2Name + ", enter the number:");
+			p2Num = scan.nextInt();
+			if (p1Num == randomNumber && p2Num == randomNumber) {
+				System.out.println("Both of you are right! It is " + randomNumber + ". Congratulations!");
+				resume = "no";
+			} else if (p1Num == randomNumber) {
+				System.out.println(p1Name + ", you are right! It is " + randomNumber + ". Congratulations!");
+				resume = "no";
+			} else if (p2Num == randomNumber) {
+				System.out.println(p2Name + ", you are right! It is " + randomNumber + ". Congratulations!");
+				resume = "no";
 			} else {
-				System.out.println(p1Name + ", your number is too small");
+				if (p1Num > randomNumber) {
+					System.out.println(p1Name + ", your number is too big");
+				} else {
+					System.out.println(p1Name + ", your number is too small");
+				}
+				if (p2Num > randomNumber) {
+					System.out.println(p2Name + ", your number is too big");
+				} else {
+					System.out.println(p2Name + ", your number is too small");
+				}
+				resume = "question";
 			}
-			if (p2Num > randomNumber) {
-				System.out.println(p2Name + ", your number is too big");
-			} else {
-				System.out.println(p2Name + ", your number is too small");
+			while (!"yes".equals(resume) && !"no".equals(resume)) {
+				System.out.println("Want to resume? 'yes' or 'no'");
+				resume = scan.next();
 			}
-			resume = "question";
-		}	
+		}
 	}
 }
--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
+
 public class GuessNumberTest {
 	public static void main(String[] args) {
-		int randomNumber = (int) (Math.random() * 101);
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter the name of the first Player: ");
 		Player p1 = new Player(scan.nextLine());
@@ -9,17 +9,7 @@ public class GuessNumberTest {
 		Player p2 = new Player(scan.nextLine());
 		System.out.println("Okay! " + p1.getName() + " and " + p2.getName() + ", let's play!");
 
-		GuessNumber game = new GuessNumber(randomNumber);
-		while ("yes".equals(game.getResume())) {
-			System.out.println(p1.getName() + ", enter the number:");
-			p1.setPlayerNum(scan.nextInt());
-			System.out.println(p2.getName() + ", enter the number:");
-			p2.setPlayerNum(scan.nextInt());
-			game.check(p1.getPlayerNum(), p2.getPlayerNum(), p1.getName(), p2.getName());
-			while (!"yes".equals(game.getResume()) && !"no".equals(game.getResume())) {
-				System.out.println("Want to resume? 'yes' or 'no'");
-				game.setResume(scan.next());
-			}
-		}
+		GuessNumber play = new GuessNumber(p1.getName(), p2.getName());
+		play.game();
 	}
 }
