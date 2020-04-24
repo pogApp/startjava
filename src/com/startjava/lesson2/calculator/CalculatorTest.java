620d04f2e9d2da61763803d8f009427d4d7b3642
pugApp <arbitmon@gmail.com>
2020-04-24 14:42:56 +0700

    Calculator array

--- src/com/startjava/lesson2/calculator/CalculatorTest.java
+++ src/com/startjava/lesson2/calculator/CalculatorTest.java
@@ -3,20 +3,20 @@ package com.startjava.lesson2.calculator;
 import java.util.Scanner;
 
 public class CalculatorTest {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String resume = "yes";
-		String operation;
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String resume = "yes";
+        String operation;
 
-		while ("yes".equals(resume)) {
-			System.out.print("What do you want to calculate? Enter: ");
-			operation = scan.nextLine();
-			Calculator calc = new Calculator(operation);
-			calc.calculate();
-			do {
-				System.out.println("Do you want to resume? 'yes' or 'no'");
-				resume = scan.nextLine();
-			} while (!"yes".equals(resume) && !"no".equals(resume));
-		}
-	}
+        while ("yes".equals(resume)) {
+            System.out.print("What do you want to calculate? Enter: ");
+            operation = scan.nextLine();
+            Calculator calc = new Calculator(operation);
+            calc.calculate();
+            do {
+                System.out.println("Do you want to resume? 'yes' or 'no'");
+                resume = scan.nextLine();
+            } while (!"yes".equals(resume) && !"no".equals(resume));
+        }
+    }
 }
