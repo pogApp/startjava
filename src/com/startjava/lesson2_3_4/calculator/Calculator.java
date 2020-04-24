2b373842479828adc9743cc92bb72e9b7ed16600
pugApp <arbitmon@gmail.com>
2020-04-24 21:43:37 +0700

    Calculator array

--- src/com/startjava/lesson2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson2_3_4/calculator/Calculator.java
@@ -1,26 +1,26 @@
 package com.startjava.lesson2_3_4.calculator;
 
 public class Calculator {
-	private String[] operation;
+	private String[] splitMathExpression;
 
-	public Calculator(String operation) {
-		this.operation = operation.split(" ");
+	public Calculator(String mathExpression) {
+		this.splitMathExpression = mathExpression.split(" ");
 	}
 
 	public void calculate() {
-		switch (operation[1]) {
-			case "+": System.out.println("Result = " + (Integer.parseInt(operation[0]) + Integer.parseInt(operation[2])));
+		switch (splitMathExpression[1]) {
+			case "+": System.out.println("Result = " + (Integer.parseInt(splitMathExpression[0]) + Integer.parseInt(splitMathExpression[2])));
 				break;
-			case "-": System.out.println("Result = " + (Integer.parseInt(operation[0]) - Integer.parseInt(operation[2])));
+			case "-": System.out.println("Result = " + (Integer.parseInt(splitMathExpression[0]) - Integer.parseInt(splitMathExpression[2])));
 				break;
-			case "*": System.out.println("Result = " + Integer.parseInt(operation[0]) * Integer.parseInt(operation[2]));
+			case "*": System.out.println("Result = " + Integer.parseInt(splitMathExpression[0]) * Integer.parseInt(splitMathExpression[2]));
 				break;
-			case "/": System.out.println("Result = " + (double) Integer.parseInt(operation[0]) / Integer.parseInt(operation[2]));
+			case "/": System.out.println("Result = " + (double) Integer.parseInt(splitMathExpression[0]) / Integer.parseInt(splitMathExpression[2]));
 				break;
 			case "^":
-				System.out.println("Result = " + Math.pow(Integer.parseInt(operation[0]), Integer.parseInt(operation[2])));
+				System.out.println("Result = " + Math.pow(Integer.parseInt(splitMathExpression[0]), Integer.parseInt(splitMathExpression[2])));
 				break;
-			case "%": System.out.println("Result = " + Integer.parseInt(operation[0]) % Integer.parseInt(operation[2]));
+			case "%": System.out.println("Result = " + Integer.parseInt(splitMathExpression[0]) % Integer.parseInt(splitMathExpression[2]));
 				break;
 			default: System.out.println("Math operator is wrong!");
 		}
--- src/com/startjava/lesson2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson2_3_4/calculator/CalculatorTest.java
@@ -6,12 +6,12 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String resume = "yes";
-        String operation;
+        String mathExpression;
 
         while ("yes".equals(resume)) {
             System.out.print("What do you want to calculate? Enter: ");
-            operation = scan.nextLine();
-            Calculator calc = new Calculator(operation);
+            mathExpression = scan.nextLine();
+            Calculator calc = new Calculator(mathExpression);
             calc.calculate();
             do {
                 System.out.println("Do you want to resume? 'yes' or 'no'");
